20ef5ee73903aa660904d118759f42f718c4baf8
SeungTaek Lim <namest504@gmail.com>
2025-11-03 22:52:29 +0900

    Update v0.1.1-test

--- Formula/termtype.rb
+++ Formula/termtype.rb
@@ -4,8 +4,8 @@
 class Termtype < Formula
   desc "A simple typing practice application for your terminal."
   homepage "https://github.com/namest504/termtype"
-  url "https://github.com/namest504/termtype/archive/refs/tags/v0.1.0-test.tar.gz"
-  sha256 "297fecaeae584379c468a057688bcacd2738ca7144dbe210efdd66b79a487b02"
+  url "https://github.com/namest504/termtype/archive/refs/tags/v0.1.1-test.tar.gz"
+  sha256 "b2a60e9645cc139f8b8c7f478404bf58327fd47a3737c2146932b27a647c07a7"
   license "MIT"
 
   depends_on "go" => :build
